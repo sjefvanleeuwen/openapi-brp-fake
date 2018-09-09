8c79b9f4d4ccddc8a13837ad4723c09e99b04404
Sjef van Leeuwen <sjef.van.leeuwen@live.nl>
2018-09-09 12:37:48 +0200

    Adding Metrics Timer for RaadpleegIngeschrevenPersoonNAW

---
+++ src/openapi-brp-fake/Metrics/Registry.cs
@@ -0,0 +1,63 @@
+using App.Metrics;
+using App.Metrics.Apdex;
+using App.Metrics.Counter;
+using App.Metrics.Gauge;
+using App.Metrics.Histogram;
+using App.Metrics.Meter;
+using App.Metrics.ReservoirSampling.ExponentialDecay;
+using App.Metrics.ReservoirSampling.Uniform;
+using App.Metrics.Timer;
+
+namespace openapi_brp_fake.Metrics
+{
+public static class Registry
+    {
+        public static TimerOptions RaadpleegIngeschrevenPersoonNAWTimer => new TimerOptions
+        {
+            Name = "RaadpleegIngeschrevenPersoonNAW",
+            MeasurementUnit = Unit.Items,
+            DurationUnit = TimeUnit.Milliseconds,
+            RateUnit = TimeUnit.Milliseconds,
+            Reservoir = () => new DefaultForwardDecayingReservoir(sampleSize: 1028, alpha: 0.015)
+        };
+        /* Common used options
+        public static GaugeOptions Errors => new GaugeOptions
+        {
+            Name = "Errors"
+        };
+
+        public static CounterOptions SampleCounter => new CounterOptions
+        {
+            Name = "Sample Counter",
+            MeasurementUnit = Unit.Calls,
+        };
+
+        public static HistogramOptions SampleHistogram => new HistogramOptions
+        {
+            Name = "Sample Histogram",
+            Reservoir = () => new DefaultAlgorithmRReservoir(),
+            MeasurementUnit = Unit.MegaBytes
+        };
+
+        public static MeterOptions SampleMeter => new MeterOptions
+        {
+            Name = "Sample Meter",
+            MeasurementUnit = Unit.Calls
+        };
+
+        public static TimerOptions SampleTimer => new TimerOptions
+        {
+            Name = "Sample Timer",
+            MeasurementUnit = Unit.Items,
+            DurationUnit = TimeUnit.Milliseconds,
+            RateUnit = TimeUnit.Milliseconds,
+            Reservoir = () => new DefaultForwardDecayingReservoir(sampleSize: 1028, alpha: 0.015)
+        };
+
+        public static ApdexOptions SampleApdex => new ApdexOptions
+        {
+            Name = "Sample Apdex"
+        };
+         */
+    }
+}
